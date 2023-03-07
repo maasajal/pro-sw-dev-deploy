c906d012e5a359ecdac62efd13c12adbf30f2de5
MD ABDULLAH ALL SAJAL <abdullah.sajal@tuni.fi>
2023-03-07 22:08:20 +0200

    Second commit as Testing automate deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Testing from "./components/Testing";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Testing />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Deploy Automate</h1>
         <code>Terminal: npm run build</code>
---
+++ src/components/Testing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Testing = () => {
+  return (
+    <>
+      <h1>Deployment project automatic</h1>
+      <p>Testing the automate deploy process by push the code to the GitHub</p>
+    </>
+  );
+};
+
+export default Testing;
