7506d548efe03d598b263132199d771d947f1709
MD ABDULLAH ALL SAJAL <abdullah.sajal@tuni.fi>
2023-03-15 12:14:45 +0200

    testing deployment

--- src/components/Testing.js
+++ src/components/Testing.js
@@ -3,6 +3,7 @@ import React from "react";
 const Testing = () => {
   return (
     <>
+      <h1> Testing deployment </h1>
       <h1>Deployment project automatic</h1>
       <p>Testing the automate deploy process by push the code to the GitHub</p>
     </>
